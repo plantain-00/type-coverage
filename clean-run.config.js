6bebfd5f9ecc843b0aa310779eb6c5a077668795
york yao <yaoao12306@outlook.com>
2018-05-10 06:55:55 +0800

    build: add test script so I can run the CLI before released to npm

---
+++ clean-run.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  include: [
+    'bin/*',
+    'dist/*.js',
+    'package.json',
+    'yarn.lock'
+  ],
+  exclude: [
+  ],
+  postScript: [
+    'cd "[dir]" && yarn --production',
+    '[dir]/bin/type-coverage -p src'
+  ]
+}
