e01065747af9f487d5b51e1762d56afe33265a38
York Yao <me@yorkyao.com>
2020-06-21 20:47:36 +0800

    refactor: improve performance

--- packages/core/src/checker.ts
+++ packages/core/src/checker.ts
@@ -97,6 +97,14 @@ export function checkNode(node: ts.Node | undefined, context: FileContext): void
   checkNodes(node.decorators, context)
   checkNodes(node.modifiers, context)
 
+  if (skippedNodeKinds.has(node.kind)) {
+    return
+  }
+
+  if (node.kind === ts.SyntaxKind.ThisKeyword){
+    collectData(node, context)
+    return
+  }
   if (ts.isIdentifier(node)) {
     if (context.catchVariables[node.escapedText as string]) {
       return
@@ -556,202 +564,195 @@ export function checkNode(node: ts.Node | undefined, context: FileContext): void
     checkNode(node.objectAssignmentInitializer, context)
     return
   }
-
-  switch (node.kind) {
-    case ts.SyntaxKind.Unknown:
-    case ts.SyntaxKind.EndOfFileToken:
-    case ts.SyntaxKind.SingleLineCommentTrivia:
-    case ts.SyntaxKind.MultiLineCommentTrivia:
-    case ts.SyntaxKind.NewLineTrivia:
-    case ts.SyntaxKind.WhitespaceTrivia:
-    case ts.SyntaxKind.ShebangTrivia:
-    case ts.SyntaxKind.ConflictMarkerTrivia:
-    case ts.SyntaxKind.NumericLiteral:
-    case ts.SyntaxKind.StringLiteral:
-    case ts.SyntaxKind.JsxText:
-    case ts.SyntaxKind.JsxTextAllWhiteSpaces:
-    case ts.SyntaxKind.RegularExpressionLiteral:
-    case ts.SyntaxKind.NoSubstitutionTemplateLiteral:
-    case ts.SyntaxKind.TemplateHead:
-    case ts.SyntaxKind.TemplateMiddle:
-    case ts.SyntaxKind.TemplateTail:
-    case ts.SyntaxKind.OpenBraceToken:
-    case ts.SyntaxKind.CloseBraceToken:
-    case ts.SyntaxKind.OpenParenToken:
-    case ts.SyntaxKind.CloseParenToken:
-    case ts.SyntaxKind.OpenBracketToken:
-    case ts.SyntaxKind.CloseBracketToken:
-    case ts.SyntaxKind.DotToken:
-    case ts.SyntaxKind.DotDotDotToken:
-    case ts.SyntaxKind.SemicolonToken:
-    case ts.SyntaxKind.CommaToken:
-    case ts.SyntaxKind.LessThanToken:
-    case ts.SyntaxKind.LessThanSlashToken:
-    case ts.SyntaxKind.GreaterThanToken:
-    case ts.SyntaxKind.LessThanEqualsToken:
-    case ts.SyntaxKind.GreaterThanEqualsToken:
-    case ts.SyntaxKind.EqualsEqualsToken:
-    case ts.SyntaxKind.ExclamationEqualsToken:
-    case ts.SyntaxKind.EqualsEqualsEqualsToken:
-    case ts.SyntaxKind.ExclamationEqualsEqualsToken:
-    case ts.SyntaxKind.EqualsGreaterThanToken:
-    case ts.SyntaxKind.PlusToken:
-    case ts.SyntaxKind.MinusToken:
-    case ts.SyntaxKind.AsteriskToken:
-    case ts.SyntaxKind.AsteriskAsteriskToken:
-    case ts.SyntaxKind.SlashToken:
-    case ts.SyntaxKind.PercentToken:
-    case ts.SyntaxKind.PlusPlusToken:
-    case ts.SyntaxKind.MinusMinusToken:
-    case ts.SyntaxKind.LessThanLessThanToken:
-    case ts.SyntaxKind.GreaterThanGreaterThanToken:
-    case ts.SyntaxKind.GreaterThanGreaterThanGreaterThanToken:
-    case ts.SyntaxKind.AmpersandToken:
-    case ts.SyntaxKind.BarToken:
-    case ts.SyntaxKind.CaretToken:
-    case ts.SyntaxKind.ExclamationToken:
-    case ts.SyntaxKind.TildeToken:
-    case ts.SyntaxKind.AmpersandAmpersandToken:
-    case ts.SyntaxKind.BarBarToken:
-    case ts.SyntaxKind.QuestionToken:
-    case ts.SyntaxKind.ColonToken:
-    case ts.SyntaxKind.AtToken:
-    case ts.SyntaxKind.QuestionQuestionToken:
-    case ts.SyntaxKind.EqualsToken:
-    case ts.SyntaxKind.PlusEqualsToken:
-    case ts.SyntaxKind.MinusEqualsToken:
-    case ts.SyntaxKind.AsteriskEqualsToken:
-    case ts.SyntaxKind.AsteriskAsteriskEqualsToken:
-    case ts.SyntaxKind.SlashEqualsToken:
-    case ts.SyntaxKind.PercentEqualsToken:
-    case ts.SyntaxKind.LessThanLessThanEqualsToken:
-    case ts.SyntaxKind.GreaterThanGreaterThanEqualsToken:
-    case ts.SyntaxKind.GreaterThanGreaterThanGreaterThanEqualsToken:
-    case ts.SyntaxKind.AmpersandEqualsToken:
-    case ts.SyntaxKind.BarEqualsToken:
-    case ts.SyntaxKind.CaretEqualsToken:
-    case ts.SyntaxKind.BreakKeyword:
-    case ts.SyntaxKind.CaseKeyword:
-    case ts.SyntaxKind.CatchKeyword:
-    case ts.SyntaxKind.ClassKeyword:
-    case ts.SyntaxKind.ConstKeyword:
-    case ts.SyntaxKind.ContinueKeyword:
-    case ts.SyntaxKind.DebuggerKeyword:
-    case ts.SyntaxKind.DefaultKeyword:
-    case ts.SyntaxKind.DeleteKeyword:
-    case ts.SyntaxKind.DoKeyword:
-    case ts.SyntaxKind.ElseKeyword:
-    case ts.SyntaxKind.EnumKeyword:
-    case ts.SyntaxKind.ExportKeyword:
-    case ts.SyntaxKind.ExtendsKeyword:
-    case ts.SyntaxKind.FalseKeyword:
-    case ts.SyntaxKind.FinallyKeyword:
-    case ts.SyntaxKind.ForKeyword:
-    case ts.SyntaxKind.FunctionKeyword:
-    case ts.SyntaxKind.IfKeyword:
-    case ts.SyntaxKind.ImportKeyword:
-    case ts.SyntaxKind.InKeyword:
-    case ts.SyntaxKind.InstanceOfKeyword:
-    case ts.SyntaxKind.NewKeyword:
-    case ts.SyntaxKind.NullKeyword:
-    case ts.SyntaxKind.ReturnKeyword:
-    case ts.SyntaxKind.SuperKeyword:
-    case ts.SyntaxKind.SwitchKeyword:
-      break
-    case ts.SyntaxKind.ThisKeyword:
-      collectData(node, context)
-      break
-    case ts.SyntaxKind.ThrowKeyword:
-    case ts.SyntaxKind.TrueKeyword:
-    case ts.SyntaxKind.TryKeyword:
-    case ts.SyntaxKind.TypeOfKeyword:
-    case ts.SyntaxKind.VarKeyword:
-    case ts.SyntaxKind.VoidKeyword:
-    case ts.SyntaxKind.WhileKeyword:
-    case ts.SyntaxKind.WithKeyword:
-    case ts.SyntaxKind.ImplementsKeyword:
-    case ts.SyntaxKind.InterfaceKeyword:
-    case ts.SyntaxKind.LetKeyword:
-    case ts.SyntaxKind.PackageKeyword:
-    case ts.SyntaxKind.PrivateKeyword:
-    case ts.SyntaxKind.ProtectedKeyword:
-    case ts.SyntaxKind.PublicKeyword:
-    case ts.SyntaxKind.StaticKeyword:
-    case ts.SyntaxKind.YieldKeyword:
-    case ts.SyntaxKind.AbstractKeyword:
-    case ts.SyntaxKind.AsKeyword:
-    case ts.SyntaxKind.AnyKeyword:
-    case ts.SyntaxKind.AsyncKeyword:
-    case ts.SyntaxKind.AwaitKeyword:
-    case ts.SyntaxKind.BooleanKeyword:
-    case ts.SyntaxKind.ConstructorKeyword:
-    case ts.SyntaxKind.DeclareKeyword:
-    case ts.SyntaxKind.GetKeyword:
-    case ts.SyntaxKind.IsKeyword:
-    case ts.SyntaxKind.KeyOfKeyword:
-    case ts.SyntaxKind.ModuleKeyword:
-    case ts.SyntaxKind.NamespaceKeyword:
-    case ts.SyntaxKind.NeverKeyword:
-    case ts.SyntaxKind.ReadonlyKeyword:
-    case ts.SyntaxKind.RequireKeyword:
-    case ts.SyntaxKind.NumberKeyword:
-    case ts.SyntaxKind.ObjectKeyword:
-    case ts.SyntaxKind.SetKeyword:
-    case ts.SyntaxKind.StringKeyword:
-    case ts.SyntaxKind.SymbolKeyword:
-    case ts.SyntaxKind.TypeKeyword:
-    case ts.SyntaxKind.UndefinedKeyword:
-    case ts.SyntaxKind.UniqueKeyword:
-    case ts.SyntaxKind.UnknownKeyword:
-    case ts.SyntaxKind.FromKeyword:
-    case ts.SyntaxKind.GlobalKeyword:
-    case ts.SyntaxKind.BigIntKeyword:
-    case ts.SyntaxKind.OfKeyword:
-    case ts.SyntaxKind.OptionalType:
-      break
-    case ts.SyntaxKind.RestType:
-      const restTypeNode = node as ts.RestTypeNode
-      checkNode(restTypeNode.type, context)
-      break
-    case ts.SyntaxKind.ThisType:
-    case ts.SyntaxKind.OmittedExpression:
-    case ts.SyntaxKind.EmptyStatement:
-    case ts.SyntaxKind.ContinueStatement:
-    case ts.SyntaxKind.BreakStatement:
-    case ts.SyntaxKind.DebuggerStatement:
-    case ts.SyntaxKind.JsxOpeningFragment:
-    case ts.SyntaxKind.JsxClosingFragment:
-    case ts.SyntaxKind.EnumMember:
-    case ts.SyntaxKind.SourceFile:
-    case ts.SyntaxKind.Bundle:
-    case ts.SyntaxKind.JSDocTypeExpression:
-    case ts.SyntaxKind.JSDocAllType:
-    case ts.SyntaxKind.JSDocUnknownType:
-    case ts.SyntaxKind.JSDocNullableType:
-    case ts.SyntaxKind.JSDocNonNullableType:
-    case ts.SyntaxKind.JSDocOptionalType:
-    case ts.SyntaxKind.JSDocFunctionType:
-    case ts.SyntaxKind.JSDocVariadicType:
-    case ts.SyntaxKind.JSDocComment:
-    case ts.SyntaxKind.JSDocTag:
-    case ts.SyntaxKind.JSDocAugmentsTag:
-    case ts.SyntaxKind.JSDocClassTag:
-    case ts.SyntaxKind.JSDocParameterTag:
-    case ts.SyntaxKind.JSDocReturnTag:
-    case ts.SyntaxKind.JSDocTypeTag:
-    case ts.SyntaxKind.JSDocTemplateTag:
-    case ts.SyntaxKind.JSDocTypedefTag:
-    case ts.SyntaxKind.JSDocPropertyTag:
-    case ts.SyntaxKind.JSDocTypeLiteral:
-    case ts.SyntaxKind.SyntaxList:
-    case ts.SyntaxKind.NotEmittedStatement:
-    case ts.SyntaxKind.PartiallyEmittedExpression:
-    case ts.SyntaxKind.CommaListExpression:
-    case ts.SyntaxKind.MergeDeclarationMarker:
-    case ts.SyntaxKind.EndOfDeclarationMarker:
-    case ts.SyntaxKind.Count:
-      break
-    default:
-      console.log(`warning: unhandled node kind: ${node.kind}`)
+  if (node.kind === ts.SyntaxKind.RestType) {
+    checkNode((node as ts.RestTypeNode).type, context)
+    return
   }
+  console.log(`warning: unhandled node kind: ${node.kind}`)
 }
+
+const skippedNodeKinds = new Set([
+  ts.SyntaxKind.Unknown,
+  ts.SyntaxKind.EndOfFileToken,
+  ts.SyntaxKind.SingleLineCommentTrivia,
+  ts.SyntaxKind.MultiLineCommentTrivia,
+  ts.SyntaxKind.NewLineTrivia,
+  ts.SyntaxKind.WhitespaceTrivia,
+  ts.SyntaxKind.ShebangTrivia,
+  ts.SyntaxKind.ConflictMarkerTrivia,
+  ts.SyntaxKind.NumericLiteral,
+  ts.SyntaxKind.StringLiteral,
+  ts.SyntaxKind.JsxText,
+  ts.SyntaxKind.JsxTextAllWhiteSpaces,
+  ts.SyntaxKind.RegularExpressionLiteral,
+  ts.SyntaxKind.NoSubstitutionTemplateLiteral,
+  ts.SyntaxKind.TemplateHead,
+  ts.SyntaxKind.TemplateMiddle,
+  ts.SyntaxKind.TemplateTail,
+  ts.SyntaxKind.OpenBraceToken,
+  ts.SyntaxKind.CloseBraceToken,
+  ts.SyntaxKind.OpenParenToken,
+  ts.SyntaxKind.CloseParenToken,
+  ts.SyntaxKind.OpenBracketToken,
+  ts.SyntaxKind.CloseBracketToken,
+  ts.SyntaxKind.DotToken,
+  ts.SyntaxKind.DotDotDotToken,
+  ts.SyntaxKind.SemicolonToken,
+  ts.SyntaxKind.CommaToken,
+  ts.SyntaxKind.LessThanToken,
+  ts.SyntaxKind.LessThanSlashToken,
+  ts.SyntaxKind.GreaterThanToken,
+  ts.SyntaxKind.LessThanEqualsToken,
+  ts.SyntaxKind.GreaterThanEqualsToken,
+  ts.SyntaxKind.EqualsEqualsToken,
+  ts.SyntaxKind.ExclamationEqualsToken,
+  ts.SyntaxKind.EqualsEqualsEqualsToken,
+  ts.SyntaxKind.ExclamationEqualsEqualsToken,
+  ts.SyntaxKind.EqualsGreaterThanToken,
+  ts.SyntaxKind.PlusToken,
+  ts.SyntaxKind.MinusToken,
+  ts.SyntaxKind.AsteriskToken,
+  ts.SyntaxKind.AsteriskAsteriskToken,
+  ts.SyntaxKind.SlashToken,
+  ts.SyntaxKind.PercentToken,
+  ts.SyntaxKind.PlusPlusToken,
+  ts.SyntaxKind.MinusMinusToken,
+  ts.SyntaxKind.LessThanLessThanToken,
+  ts.SyntaxKind.GreaterThanGreaterThanToken,
+  ts.SyntaxKind.GreaterThanGreaterThanGreaterThanToken,
+  ts.SyntaxKind.AmpersandToken,
+  ts.SyntaxKind.BarToken,
+  ts.SyntaxKind.CaretToken,
+  ts.SyntaxKind.ExclamationToken,
+  ts.SyntaxKind.TildeToken,
+  ts.SyntaxKind.AmpersandAmpersandToken,
+  ts.SyntaxKind.BarBarToken,
+  ts.SyntaxKind.QuestionToken,
+  ts.SyntaxKind.ColonToken,
+  ts.SyntaxKind.AtToken,
+  ts.SyntaxKind.QuestionQuestionToken,
+  ts.SyntaxKind.EqualsToken,
+  ts.SyntaxKind.PlusEqualsToken,
+  ts.SyntaxKind.MinusEqualsToken,
+  ts.SyntaxKind.AsteriskEqualsToken,
+  ts.SyntaxKind.AsteriskAsteriskEqualsToken,
+  ts.SyntaxKind.SlashEqualsToken,
+  ts.SyntaxKind.PercentEqualsToken,
+  ts.SyntaxKind.LessThanLessThanEqualsToken,
+  ts.SyntaxKind.GreaterThanGreaterThanEqualsToken,
+  ts.SyntaxKind.GreaterThanGreaterThanGreaterThanEqualsToken,
+  ts.SyntaxKind.AmpersandEqualsToken,
+  ts.SyntaxKind.BarEqualsToken,
+  ts.SyntaxKind.CaretEqualsToken,
+  ts.SyntaxKind.BreakKeyword,
+  ts.SyntaxKind.CaseKeyword,
+  ts.SyntaxKind.CatchKeyword,
+  ts.SyntaxKind.ClassKeyword,
+  ts.SyntaxKind.ConstKeyword,
+  ts.SyntaxKind.ContinueKeyword,
+  ts.SyntaxKind.DebuggerKeyword,
+  ts.SyntaxKind.DefaultKeyword,
+  ts.SyntaxKind.DeleteKeyword,
+  ts.SyntaxKind.DoKeyword,
+  ts.SyntaxKind.ElseKeyword,
+  ts.SyntaxKind.EnumKeyword,
+  ts.SyntaxKind.ExportKeyword,
+  ts.SyntaxKind.ExtendsKeyword,
+  ts.SyntaxKind.FalseKeyword,
+  ts.SyntaxKind.FinallyKeyword,
+  ts.SyntaxKind.ForKeyword,
+  ts.SyntaxKind.FunctionKeyword,
+  ts.SyntaxKind.IfKeyword,
+  ts.SyntaxKind.ImportKeyword,
+  ts.SyntaxKind.InKeyword,
+  ts.SyntaxKind.InstanceOfKeyword,
+  ts.SyntaxKind.NewKeyword,
+  ts.SyntaxKind.NullKeyword,
+  ts.SyntaxKind.ReturnKeyword,
+  ts.SyntaxKind.SuperKeyword,
+  ts.SyntaxKind.SwitchKeyword,
+  ts.SyntaxKind.ThrowKeyword,
+  ts.SyntaxKind.TrueKeyword,
+  ts.SyntaxKind.TryKeyword,
+  ts.SyntaxKind.TypeOfKeyword,
+  ts.SyntaxKind.VarKeyword,
+  ts.SyntaxKind.VoidKeyword,
+  ts.SyntaxKind.WhileKeyword,
+  ts.SyntaxKind.WithKeyword,
+  ts.SyntaxKind.ImplementsKeyword,
+  ts.SyntaxKind.InterfaceKeyword,
+  ts.SyntaxKind.LetKeyword,
+  ts.SyntaxKind.PackageKeyword,
+  ts.SyntaxKind.PrivateKeyword,
+  ts.SyntaxKind.ProtectedKeyword,
+  ts.SyntaxKind.PublicKeyword,
+  ts.SyntaxKind.StaticKeyword,
+  ts.SyntaxKind.YieldKeyword,
+  ts.SyntaxKind.AbstractKeyword,
+  ts.SyntaxKind.AsKeyword,
+  ts.SyntaxKind.AnyKeyword,
+  ts.SyntaxKind.AsyncKeyword,
+  ts.SyntaxKind.AwaitKeyword,
+  ts.SyntaxKind.BooleanKeyword,
+  ts.SyntaxKind.ConstructorKeyword,
+  ts.SyntaxKind.DeclareKeyword,
+  ts.SyntaxKind.GetKeyword,
+  ts.SyntaxKind.IsKeyword,
+  ts.SyntaxKind.KeyOfKeyword,
+  ts.SyntaxKind.ModuleKeyword,
+  ts.SyntaxKind.NamespaceKeyword,
+  ts.SyntaxKind.NeverKeyword,
+  ts.SyntaxKind.ReadonlyKeyword,
+  ts.SyntaxKind.RequireKeyword,
+  ts.SyntaxKind.NumberKeyword,
+  ts.SyntaxKind.ObjectKeyword,
+  ts.SyntaxKind.SetKeyword,
+  ts.SyntaxKind.StringKeyword,
+  ts.SyntaxKind.SymbolKeyword,
+  ts.SyntaxKind.TypeKeyword,
+  ts.SyntaxKind.UndefinedKeyword,
+  ts.SyntaxKind.UniqueKeyword,
+  ts.SyntaxKind.UnknownKeyword,
+  ts.SyntaxKind.FromKeyword,
+  ts.SyntaxKind.GlobalKeyword,
+  ts.SyntaxKind.BigIntKeyword,
+  ts.SyntaxKind.OfKeyword,
+  ts.SyntaxKind.OptionalType,
+  ts.SyntaxKind.ThisType,
+  ts.SyntaxKind.OmittedExpression,
+  ts.SyntaxKind.EmptyStatement,
+  ts.SyntaxKind.ContinueStatement,
+  ts.SyntaxKind.BreakStatement,
+  ts.SyntaxKind.DebuggerStatement,
+  ts.SyntaxKind.JsxOpeningFragment,
+  ts.SyntaxKind.JsxClosingFragment,
+  ts.SyntaxKind.EnumMember,
+  ts.SyntaxKind.SourceFile,
+  ts.SyntaxKind.Bundle,
+  ts.SyntaxKind.JSDocTypeExpression,
+  ts.SyntaxKind.JSDocAllType,
+  ts.SyntaxKind.JSDocUnknownType,
+  ts.SyntaxKind.JSDocNullableType,
+  ts.SyntaxKind.JSDocNonNullableType,
+  ts.SyntaxKind.JSDocOptionalType,
+  ts.SyntaxKind.JSDocFunctionType,
+  ts.SyntaxKind.JSDocVariadicType,
+  ts.SyntaxKind.JSDocComment,
+  ts.SyntaxKind.JSDocTag,
+  ts.SyntaxKind.JSDocAugmentsTag,
+  ts.SyntaxKind.JSDocClassTag,
+  ts.SyntaxKind.JSDocParameterTag,
+  ts.SyntaxKind.JSDocReturnTag,
+  ts.SyntaxKind.JSDocTypeTag,
+  ts.SyntaxKind.JSDocTemplateTag,
+  ts.SyntaxKind.JSDocTypedefTag,
+  ts.SyntaxKind.JSDocPropertyTag,
+  ts.SyntaxKind.JSDocTypeLiteral,
+  ts.SyntaxKind.SyntaxList,
+  ts.SyntaxKind.NotEmittedStatement,
+  ts.SyntaxKind.PartiallyEmittedExpression,
+  ts.SyntaxKind.CommaListExpression,
+  ts.SyntaxKind.MergeDeclarationMarker,
+  ts.SyntaxKind.EndOfDeclarationMarker,
+  ts.SyntaxKind.Count,
+])
